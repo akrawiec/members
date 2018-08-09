51706757c9f690e813ffb4f693f968db264e8c7d
Adam Krawiec <akrawiec@santanderconsumerusa.com>
2018-08-09 10:04:14 -0500

    L20, 21, 25, 26 - We should use double curly brackets for escaping via Moustache

--- admin/tmpl/cap-control.php
+++ admin/tmpl/cap-control.php
@@ -17,12 +17,12 @@
 	</td>
 
 	<td class="column-grant">
-		<span class="screen-reader-text">{{{ data.label.grant }}}</span>
-		<input {{{ data.readonly }}} type="checkbox" name="{{ data.name.grant }}" data-grant-cap="{{ data.cap }}" value="{{ data.cap }}" <# if ( data.is_granted_cap ) { #>checked="checked"<# } #> />
+		<span class="screen-reader-text">{{ data.label.grant }}</span>
+		<input {{ data.readonly }} type="checkbox" name="{{ data.name.grant }}" data-grant-cap="{{ data.cap }}" value="{{ data.cap }}" <# if ( data.is_granted_cap ) { #>checked="checked"<# } #> />
 	</td>
 
 	<td class="column-deny">
-		<span class="screen-reader-text">{{{ data.label.deny }}}</span>
-		<input {{{ data.readonly }}} type="checkbox" name="{{ data.name.deny }}" data-deny-cap="{{ data.cap }}" value="{{ data.cap }}" <# if ( data.is_denied_cap ) { #>checked="checked"<# } #> />
+		<span class="screen-reader-text">{{ data.label.deny }}</span>
+		<input {{ data.readonly }} type="checkbox" name="{{ data.name.deny }}" data-deny-cap="{{ data.cap }}" value="{{ data.cap }}" <# if ( data.is_denied_cap ) { #>checked="checked"<# } #> />
 	</td>
 </tr>
